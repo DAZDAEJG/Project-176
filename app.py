e50d874281223afcb24b640698a4a8ecce43ed4d
DAZDAEJG <92121569+DAZDAEJG@users.noreply.github.com>
2023-11-07 19:51:40 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, jsonify, request
+import random
+
+app = Flask(__name__)
+
+answer_dict={
+                "1": ["Gran", "Gorilla", "Negro", "Madagascar", "Agradable", "Tigres", "Blancos", "Moverme"],
+                "2": ["Domingo", "Tía", "Perro", "Hamburguesas", "Refrescos", "Agradable", "Cartas"],
+                "3": ["Gato", "Apestoso", "California", "Gato", "Azul", "3", "Peces", "Bailar", "Canciones", "Triste", "Infantil", "Feliz"]
+            }
+
+
+
+
+#¡Añade aquí los datos de las historias!
+
+
+
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+    
+
+#Añade aquí la API "/get-story".
+
+
+    
+
+
+@app.route("/post-answers", methods=["POST"])
+def post_answers():
+    story_id = request.json.get("story_id")
+    values = request.json.get("values")
+    answers = answer_dict.get(story_id)
+    index, score = 0, 0
+    while index < len(values):
+        if values[index].lower() == answers[index].lower():
+            score += 1
+        index += 1
+    return jsonify({
+        "status": "success",
+        "result": f"{score} / {len(values)}"
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
